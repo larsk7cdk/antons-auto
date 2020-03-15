e194619f26f48ce0fd0d91beb849d23c2924d7c3
Lars Larsen <lars@k7c.dk>
2020-03-15 20:43:58 +0100

    working...

--- antons-auto.mvc/Controllers/HomeController.cs
+++ antons-auto.mvc/Controllers/HomeController.cs
@@ -1,22 +1,38 @@
 ﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using antons_auto.mvc.Data;
+using antons_auto.mvc.Data.Entities;
 using antons_auto.mvc.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace antons_auto.mvc.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context, ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var carsViewModel = await _context.Cars
+                .Include(x => x.CarModel)
+                .Include(x => x.CarModel.CarBrand)
+                .OrderBy(o => o.CarModel.CarBrand.Name)
+                .Take(3)
+                .AsNoTracking()
+                .Select(car => MapToViewModel(car))
+                .ToListAsync();
+
+            return View(carsViewModel);
         }
 
         public IActionResult Privacy()
@@ -27,7 +43,21 @@ namespace antons_auto.mvc.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        private static CarViewModel MapToViewModel(Car car) => new CarViewModel
+        {
+            CarID = car.CarID,
+            CarBrandID = car.CarModel.CarBrandID,
+            CarModelID = car.CarModelID.ToString(),
+            CarBrandName = car.CarModel.CarBrand.Name,
+            CarModelName = car.CarModel.Name,
+            Year = car.Year,
+            Price = car.Price,
+            MileAge = car.MileAge,
+            ImageUrl = car.ImageUrl
+        };
+
     }
 }
