b8a3dc14ded0aa38c8abf2d1027dd7ed93a0d1f6
Lars Larsen <lars@k7c.dk>
2020-04-05 10:02:44 +0200

    finished opgave-5

--- antons-auto.mvc/Controllers/AboutController.cs
+++ antons-auto.mvc/Controllers/AboutController.cs
@@ -1,16 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using antons_auto.mvc.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace antons_auto.mvc.Controllers
 {
     public class AboutController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public AboutController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var imageUrls = _context.Cars
+                .Include(x => x.CarModel)
+                .Include(x => x.CarModel.CarBrand)
+                .Where(s => !string.IsNullOrEmpty(s.ImageUrl))
+                .Select(s => s.ImageUrl).ToList();
+            
+            return View(imageUrls);
         }
     }
 }
